aa59226edc356a070cb74dd16e5628bf79defa5b
deepthakannan <deepthakannan@gmail.com>
2018-03-19 21:11:29 -0400

    hello dataflow with a basic actionflow

---
+++ PlayDataflow/Test/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var throwIfNegative = new ActionBlock<int>(n =>
+            {
+                Console.WriteLine("n = {0}", n);
+                if (n < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            });
+
+            // Post values to the block.
+            throwIfNegative.Post(0);
+            throwIfNegative.Post(-1);
+            throwIfNegative.Post(1);
+            throwIfNegative.Post(-2);
+            throwIfNegative.Complete();
+
+            // Wait for completion in a try/catch block.
+            try
+            {
+                throwIfNegative.Completion.Wait();
+            }
+            catch (AggregateException ae)
+            {
+                // If an unhandled exception occurs during dataflow processing, all
+                // exceptions are propagated through an AggregateException object.
+                ae.Handle(e =>
+                {
+                    Console.WriteLine("Encountered {0}: {1}",
+                       e.GetType().Name, e.Message);
+                    return true;
+                });
+            }
+        }
+    }
+}
